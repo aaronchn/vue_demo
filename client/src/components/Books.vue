98dd83e5d9a9de359149fe0c295b3944c34af49e
Aaron <aaronchn@live.com>
2018-09-23 13:56:02 +0800

    Book [DELETE]

--- client/src/components/Books.vue
+++ client/src/components/Books.vue
@@ -32,7 +32,11 @@
                         @click="editBook(book)">
                     Update
                 </button>
-                <button type="button" class="btn btn-danger btn-sm">Delete</button>
+                <button type="button"
+                          class="btn btn-danger btn-sm"
+                          @click="onDeleteBook(book)">
+                          Delete
+                </button>
               </td>
             </tr>
           </tbody>
@@ -228,6 +232,23 @@ export default {
       this.$refs.editBookModal.hide();
       this.initForm();
       this.getBooks(); // why?
+    },
+    removeBook(bookID) {
+      const path = `http://localhost:5000/books/${bookID}`;
+      axios.delete(path)
+        .then(() => {
+          this.getBooks();
+          this.message = 'Book removed!';
+          this.showMessage = true;
+        })
+        .catch((error) => {
+          // eslint-disable-next-line
+          console.error(error);
+          this.getBooks();
+        });
+    },
+    onDeleteBook(book) {
+      this.removeBook(book.id);
     },
   },
 };
