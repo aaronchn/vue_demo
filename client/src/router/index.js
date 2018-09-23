88e211e596dba70a09c6a0e9173cb06e747b0c3f
Aaron <aaronchn@live.com>
2018-09-23 09:22:07 +0800

    Book app at front-end

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue';
 import Router from 'vue-router';
 import Ping from '@/components/Ping';
+import Books from '@/components/Books';
 
 Vue.use(Router);
 
@@ -9,9 +10,15 @@ Vue.use(Router);
 export default new Router({
   routes: [
     {
+      path: '/',
+      name: 'Books',
+      component: Books,
+    },
+    {
       path: '/ping',
       name: 'Ping',
       component: Ping,
     },
   ],
+  mode:'hash',
 });
